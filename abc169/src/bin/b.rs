6d4185fbe6eee97f4d77cfd7930a913bb3070597
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2020-06-10 09:35:41 +0900

    Pass b

--- abc169/src/bin/b.rs
+++ abc169/src/bin/b.rs
@@ -1,3 +1,20 @@
+use std::io;
+use reduce::Reduce;
+
 fn main() {
-    unimplemented!();
+    let mut arg_count_str = String::new();
+    io::stdin().read_line(&mut arg_count_str).unwrap();
+    let mut args_str = String::new();
+    io::stdin().read_line(&mut args_str).unwrap();
+
+    // let arg_count: i64 = arg_count_str.parse().unwrap();
+    let args: Vec<i128> = args_str.split(" ").map(|arg| arg.parse().unwrap() ).collect();
+
+    let mut result: i128 = args.into_iter().reduce(|a, b| a * b).unwrap_or(0);
+
+    if result > (10 as i128).pow(18) {
+        result = -1;
+    }
+
+    println!("{:?}", result);
 }
