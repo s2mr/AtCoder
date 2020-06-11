ffa01a1dbb170c444e0b8f9e932b19321716ed74
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2020-06-11 10:06:30 +0900

    Pass c

--- abc169/src/bin/c.rs
+++ abc169/src/bin/c.rs
@@ -1,3 +1,10 @@
+use std::io::stdin;
+
 fn main() {
-    unimplemented!();
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+
+    let nums: Vec<f64> = input.split(" ").map(|arg| arg.parse().unwrap()).collect();
+    let result = nums[0] * nums[1];
+    println!("{:?}", result as i128);
 }
